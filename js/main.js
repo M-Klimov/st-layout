d546d17e71b443d01416f10abe542302d4ae4880
lightc0de <n0975771178@gmail.com>
2019-08-01 19:59:01 +0300

    added js tabs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-console.log('test push to master!');
+let tab, tabContent;
+
+window.onload = function() {
+    tab = document.getElementsByClassName('tab');
+    tabContent = document.getElementsByClassName('tab-content');
+    hideTabsContent(1);
+}
+
+function hideTabsContent(num) {
+    for (let i = num; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+        tab[i].classList.remove('active');
+    }
+}
+
+document.querySelector('.tabs').onclick = function(event) {
+    const target = event.target;
+    if (target.className == 'tab') {
+        for (let i = 0; i < tab.length; i++) {
+            if (target == tab[i]) {
+                showTabsContent(i);
+            }
+        }
+    }
+}
+
+function showTabsContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tab[b].classList.add('active');
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
